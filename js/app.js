85eb00efb593adaefeace2f88c052bb5a25ad82d
iLBigga <Giannanti1996@gmail.com>
2022-07-21 18:38:38 +0200

    Primo commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Come ti chiami?
+const nome = document.querySelector('input[type="nome"]')
+// Quanti chilometri devi percorrere?
+const distanza = document.querySelector('input[type="distanza"]')
+// Quanti hanni hai?
+// SE la persona ha meno di 18 hanni
+    // Sconto 20%
+// Altrimenti se la persona ha più di 65 anni
+    // Sconto 50%
+
+
+
+
+// Console log
+console.log(distanza)
